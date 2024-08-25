25025eaf71859a5fd1e959668e00715bd7370824
Prateek Taluja <prateek.taluja@whizkidzmedia.com>
2024-08-25 13:49:54 +0530

    content  changes in  homepage

--- app/page.js
+++ app/page.js
@@ -17,24 +17,23 @@ export default function SearchBar() {
   let router = useRouter();
 
   const searchParams = useSearchParams()
-  const [state, update] = useState({})
+  const [state, update] = useState('Loading content...')
 
   const search = searchParams.get('search')
-  console.log(search)
   useEffect(() => {
     get_data();
   }, []) // eslint-disable-next-line react-hooks/exhaustive-deps
 
-  async function get_data() {
+  async function get_data() {// geting data from fetch api and adding the logic if query parameter value is not present in database then show not foundnpage else show the data
     let response = await fetch('https://jsonplaceholder.typicode.com/todos/' + search)
     if (response.status === 404) {
       router.push("/not-found")
     }
     else {
       let data = await response.json();
-      update(data)
+      update(`Data: ${JSON.stringify(data)}`)
     }
   }
 
-  return <>Search: {JSON.stringify(state)}</>
+  return <>{state}</>
 }
